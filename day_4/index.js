91a6dc3d45aca28b418f179bd1773d9382045a0e
Oleg <mightyxerox@gmail.com>
2019-02-01 13:26:43 +0300

    Take zeros number from command line arguments

--- day_4/index.js
+++ day_4/index.js
@@ -8,16 +8,19 @@ function* sequence() {
 }
 
 const key = process.argv[2];
-if (!key) {
-  console.log("Key is required");
+const numberOfZeros = process.argv[3]
+if (!key || !numberOfZeros) {
+  console.log("Key and number of zeros arguments required");
   process.exit(1);
 }
+
+const zeros = "0".repeat(numberOfZeros)
 
 for (let number of sequence()) {
   const hash = crypto.createHash("md5");
   hash.update(key + number);
-  const digest = hash.digest("hex").slice(0, 5);
-  if (digest === "000000") {
+  const digest = hash.digest("hex").slice(0, numberOfZeros);
+  if (digest === zeros) {
     console.log(number);
     break;
   }
