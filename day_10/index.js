8304c37d416bd5e03a46ec6c7d302a01878665c2
Oleg <super.oleg.3000@yandex.ru>
2019-02-02 20:21:57 +0300

    Add day 10

---
+++ day_10/index.js
@@ -0,0 +1,33 @@
+function makeSequence(seq, runs) {
+  let lastDigit = null;
+  let repeats = 0;
+  let newSeq = "";
+  for (let digit of seq) {
+    if (digit !== lastDigit) {
+      if (lastDigit) {
+        newSeq += repeats;
+        newSeq += lastDigit;
+      }
+      lastDigit = digit;
+      repeats = 1;
+    } else {
+      repeats++;
+    }
+  }
+  newSeq += repeats;
+  newSeq += lastDigit;
+
+  if (runs === 1) {
+    return newSeq;
+  } else {
+    return process(newSeq, runs - 1);
+  }
+}
+
+const sequence = process.argv[2];
+const runs = process.argv[3] ? process.argv[3] : 1;
+if (!sequence) {
+  console.log("Sequence arg is missing");
+}
+
+console.log(makeSequence(sequence, runs).length);
