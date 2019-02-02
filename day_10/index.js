1fa5344586c7ebc5838297a6266c5c2f8614ddbb
Oleg <super.oleg.3000@yandex.ru>
2019-02-02 21:39:19 +0300

    Exit if no sequence argument is supplied

--- day_10/index.js
+++ day_10/index.js
@@ -1,33 +1,34 @@
-function makeSequence(seq, runs) {
-  let lastDigit = null;
-  let repeats = 0;
-  let newSeq = "";
+function makeSequence (seq, runs) {
+  let lastDigit = null
+  let repeats = 0
+  let newSeq = ''
   for (let digit of seq) {
     if (digit !== lastDigit) {
       if (lastDigit) {
-        newSeq += repeats;
-        newSeq += lastDigit;
+        newSeq += repeats
+        newSeq += lastDigit
       }
-      lastDigit = digit;
-      repeats = 1;
+      lastDigit = digit
+      repeats = 1
     } else {
-      repeats++;
+      repeats++
     }
   }
-  newSeq += repeats;
-  newSeq += lastDigit;
+  newSeq += repeats
+  newSeq += lastDigit
 
   if (runs === 1) {
-    return newSeq;
+    return newSeq
   } else {
-    return process(newSeq, runs - 1);
+    return process(newSeq, runs - 1)
   }
 }
 
-const sequence = process.argv[2];
-const runs = process.argv[3] ? process.argv[3] : 1;
+const sequence = process.argv[2]
+const runs = process.argv[3] ? process.argv[3] : 1
 if (!sequence) {
-  console.log("Sequence arg is missing");
+  console.log('Sequence arg is missing')
+  process.exit(1)
 }
 
-console.log(makeSequence(sequence, runs).length);
+console.log(makeSequence(sequence, runs).length)
