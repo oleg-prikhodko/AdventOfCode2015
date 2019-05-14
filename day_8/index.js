2bce75bdfffac6e0b2c8e8cb36bbae2177a1005b
Oleg Prikhodko <super.oleg.3000@yandex.ru>
2019-05-14 19:01:14 +0300

    Fix day 8

--- day_8/index.js
+++ day_8/index.js
@@ -1,11 +1,12 @@
 const { readFile } = require('fs').promises
 const path = require('path')
+const assert = require('assert')
 
 const escapePattern = /\\"|\\[^"]/g
 const asciiPattern = /(\\x[0-9a-f]{2})/g
 const openingQuotesCount = 2
 
-function countStringChars (str) {
+function countMemChars (str) {
   let escapeCount = 0
   let asciiCount = 0
   if (escapePattern.test(str)) {
@@ -18,29 +19,48 @@ function countStringChars (str) {
   return count
 }
 
-function encodeString (str) {
-  let encoded = str.replace(/\\{2}/g, String.raw`\\\\`)
-    .replace(/\\"(.+)/g, String.raw`\\\"$1`)
-    .replace(asciiPattern, '\\$1')
-    .replace(/^"|"$/g, '\\"')
-  encoded = `"${encoded}"`
-  return encoded
+function encode (str) {
+  const stringChars = []
+  for (let char of str) {
+    switch (char) {
+      case '"':
+        stringChars.push('\\"')
+        break
+      case '\\':
+        stringChars.push('\\\\')
+        break
+      default:
+        stringChars.push(char)
+        break
+    }
+  }
+  return `"${stringChars.join('')}"`
 }
 
-// const str = String.raw`"\"\\"`
-// console.log(
-//     str, encodeString(str)
-// )
+const strs = [
+  String.raw`""`,
+  String.raw`"abc"`,
+  String.raw`"aaa\"aaa"`,
+  String.raw`"\x27"`
+]
+const results = [
+  String.raw`"\"\""`,
+  String.raw`"\"abc\""`,
+  String.raw`"\"aaa\\\"aaa\""`,
+  String.raw`"\"\\x27\""`
+]
+strs.forEach((str, index) => assert.strictEqual(encode(str), results[index]))
 
 readFile(path.join(__dirname, 'input.txt'), 'utf8')
   .then(contents => {
     const strings = contents.trim().split('\n')
-    const encodedStrings = strings.map(str => encodeString(str))
 
-    const codeChars = strings.reduce((acc, str) => acc + str.length, 0)
-    const stringChars = strings.reduce((acc, str) => acc + countStringChars(str), 0)
-    console.log('Part one', codeChars - stringChars)
+    const lengthSum = (acc, str) => acc + str.length
+    const codeChars = strings.reduce(lengthSum, 0)
+    const memChars = strings.reduce((acc, str) => acc + countMemChars(str), 0)
+    console.log('Part one:', codeChars - memChars)
 
-    const encodedCodeChars = encodedStrings.reduce((acc, str) => acc + str.length, 0)
-    console.log('Part two', encodedCodeChars - codeChars)
+    const encodedStrings = strings.map(str => encode(str))
+    const encodedCodeChars = encodedStrings.reduce(lengthSum, 0)
+    console.log('Part two:', encodedCodeChars - codeChars)
   })
